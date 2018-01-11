1c130b252079dfd1e156902f920da9d526baf218
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-01-11 10:49:03 +0100

    renamed test script - added dgemm test

--- prometeo/linalg/test_blasfeo_ctypes.py
+++ prometeo/linalg/test_blasfeo_ctypes.py
@@ -28,28 +28,50 @@ bw.v_zeros_align(byref(memory_strmat), size_strmat)
 
 ptr_memory_strmat = cast(memory_strmat, c_char_p)
 
+A = (POINTER(c_double) * 1)()
+bw.d_zeros(byref(A), n, n)
+for i in range(n*n):
+    A[0][i] = i
+
 sA = blasfeo_dmat()
 
 bw.blasfeo_allocate_dmat(n, n, byref(sA))
 bw.blasfeo_create_dmat(n, n, byref(sA), ptr_memory_strmat)
+bw.blasfeo_pack_dmat(n, n, A[0], n, byref(sA), 0, 0);
 bw.blasfeo_print_dmat(n, n, byref(sA), 0, 0)
 
 ptr_memory_strmat = cast(ptr_memory_strmat, c_void_p)
 ptr_memory_strmat.value = ptr_memory_strmat.value + sA.memsize
 ptr_memory_strmat = cast(ptr_memory_strmat, c_char_p)
 
+D = (POINTER(c_double) * 1)()
+bw.d_zeros(byref(D), n, n)
+for i in range(n):
+    D[0][i*(n + 1)] = 1.0
+
 sD = blasfeo_dmat()
 
 bw.blasfeo_allocate_dmat(n, n, byref(sD))
 bw.blasfeo_create_dmat(n, n, byref(sD), ptr_memory_strmat)
+bw.blasfeo_pack_dmat(n, n, D[0], n, byref(sD), 0, 0);
+bw.blasfeo_print_dmat(n, n, byref(sD), 0, 0)
+
 ptr_memory_strmat = cast(ptr_memory_strmat, c_void_p)
 ptr_memory_strmat.value = ptr_memory_strmat.value + sD.memsize
 ptr_memory_strmat = cast(ptr_memory_strmat, c_char_p)
+
+B = (POINTER(c_double) * 1)()
+bw.d_zeros(byref(B), n, n)
+for i in range(n):
+    B[0][i*(n + 1)] = 1.0
 
 sB = blasfeo_dmat()
 
 bw.blasfeo_allocate_dmat(n, n, byref(sB))
 bw.blasfeo_create_dmat(n, n, byref(sB), ptr_memory_strmat)
+bw.blasfeo_pack_dmat(n, n, B[0], n, byref(sB), 0, 0);
+bw.blasfeo_print_dmat(n, n, byref(sB), 0, 0)
+
 ptr_memory_strmat = cast(ptr_memory_strmat, c_void_p)
 ptr_memory_strmat.value = ptr_memory_strmat.value + sB.memsize
 ptr_memory_strmat = cast(ptr_memory_strmat, c_char_p)
@@ -60,4 +82,5 @@ ptr_memory_strmat = cast(ptr_memory_strmat, c_char_p)
 
 #import pdb; pdb.set_trace()
 bw.blasfeo_dgemm_nt(n, n, n, 1.0, byref(sA), 0, 0, byref(sA), 0, 0, 1, byref(sB), 0, 0, byref(sD), 0, 0);
+bw.blasfeo_print_dmat(n, n, byref(sD), 0, 0)
 
