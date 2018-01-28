86fcf7975d947c9fb21c8af3e5f4c9394637c48c
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-01-28 16:13:46 +0100

    temporarily removed default __init__

--- prometeo/linalg/prmt_mat_blasfeo_wrapper.py
+++ prometeo/linalg/prmt_mat_blasfeo_wrapper.py
@@ -1,23 +1,23 @@
 from blasfeo_wrapper import *
 from ctypes import *
 
-def c_prmt_create_prmt_mat(m: int, n: int, data: POINTER(c_double)):
-         
-    size_strmat = bw.blasfeo_memsize_dmat(m, n)
-    memory_strmat = c_void_p() 
-    bw.v_zeros_align(byref(memory_strmat), size_strmat)
-
-    ptr_memory_strmat = cast(memory_strmat, c_char_p)
-
-    A = (POINTER(c_double) * 1)()
-    bw.d_zeros(byref(A), n, n)
-
-    sA = blasfeo_dmat()
-
-    bw.blasfeo_allocate_dmat(m, n, byref(sA))
-    bw.blasfeo_create_dmat(m, n, byref(sA), ptr_memory_strmat)
-    bw.blasfeo_pack_dmat(m, n, data[0], n, byref(sA), 0, 0)
-    return sA
+#def c_prmt_create_prmt_mat(m: int, n: int, data: POINTER(c_double)):
+#         
+#    size_strmat = bw.blasfeo_memsize_dmat(m, n)
+#    memory_strmat = c_void_p() 
+#    bw.v_zeros_align(byref(memory_strmat), size_strmat)
+#
+#    ptr_memory_strmat = cast(memory_strmat, c_char_p)
+#
+#    A = (POINTER(c_double) * 1)()
+#    bw.d_zeros(byref(A), n, n)
+#
+#    sA = blasfeo_dmat()
+#
+#    bw.blasfeo_allocate_dmat(m, n, byref(sA))
+#    bw.blasfeo_create_dmat(m, n, byref(sA), ptr_memory_strmat)
+#    bw.blasfeo_pack_dmat(m, n, data[0], n, byref(sA), 0, 0)
+#    return sA
 
 def c_prmt_create_prmt_mat(m: int, n: int):
          
