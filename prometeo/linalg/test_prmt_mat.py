5039733ec421d6beeaf7461664e616cb3dd4396c
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-01-28 16:03:27 +0100

    polished test

--- prometeo/linalg/test_prmt_mat.py
+++ prometeo/linalg/test_prmt_mat.py
@@ -1,28 +1,26 @@
 from prmt_mat import *
 from blasfeo_wrapper import *
 
-n = 10
+n: int = 10
 
-data_A = (POINTER(c_double) * 1)()
+void_p = int 
+
+data_A: void_p = (POINTER(c_double) * 1)()
 bw.d_zeros(byref(data_A), n, n)
 for i in range(n*n):
     data_A[0][i] = i
+A: prmt_mat = prmt_mat(n, n, data_A)
 
-A = prmt_mat(n, n, data_A)
-
-data_B = (POINTER(c_double) * 1)()
+data_B: void_p = (POINTER(c_double) * 1)()
 bw.d_zeros(byref(data_B), n, n)
-B = (POINTER(c_double) * 1)()
-bw.d_zeros(byref(B), n, n)
 for i in range(n):
     data_B[0][i*(n + 1)] = 1.0
+B: prmt_mat = prmt_mat(n, n, data_B)
 
-B = prmt_mat(n, n, data_B)
-
-data_C = (POINTER(c_double) * 1)()
+data_C: void_p = (POINTER(c_double) * 1)()
 bw.d_zeros(byref(data_C), n, n)
+C: prmt_mat = prmt_mat(n, n, data_C)
 
-C = prmt_mat(n, n, data_C)
 dgemm_nt(A, B, C, C)
 
 # print results
