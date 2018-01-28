a0a891cd0d13c316f10267218b83943a68b7e02b
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-01-28 16:30:12 +0100

    restored non-compact test

--- prometeo/linalg/test_prmt_mat.py
+++ prometeo/linalg/test_prmt_mat.py
@@ -5,22 +5,27 @@ n: int = 10
 
 void_p = int 
 
-data_A: void_p = (POINTER(c_double) * 1)()
+data_A: int = (POINTER(c_double) * 1)()
 bw.d_zeros(byref(data_A), n, n)
 for i in range(n*n):
     data_A[0][i] = i
+
 A: prmt_mat = prmt_mat(n, n, data_A)
 
 data_B: void_p = (POINTER(c_double) * 1)()
 bw.d_zeros(byref(data_B), n, n)
+B:prmt_mat = (POINTER(c_double) * 1)()
+
+bw.d_zeros(byref(B), n, n)
 for i in range(n):
     data_B[0][i*(n + 1)] = 1.0
+
 B: prmt_mat = prmt_mat(n, n, data_B)
 
 data_C: void_p = (POINTER(c_double) * 1)()
 bw.d_zeros(byref(data_C), n, n)
-C: prmt_mat = prmt_mat(n, n, data_C)
 
+C: prmt_mat = prmt_mat(n, n, data_C)
 dgemm_nt(A, B, C, C)
 
 # print results
