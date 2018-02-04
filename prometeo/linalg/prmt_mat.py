e8b2d2c9c349728b663c1102ad48077aaf226de2
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-02-04 22:52:26 +0100

    added set method for prmt_mat, chagend to slimmer syntax for void pointers

--- prometeo/linalg/prmt_mat.py
+++ prometeo/linalg/prmt_mat.py
@@ -6,11 +6,14 @@ class prmt_mat:
 
     blasfeo_dmat 
     
-#    def __init__(self, m: int, n: int, data: POINTER(c_double)):
-#        self.blasfeo_dmat = c_prmt_create_prmt_mat(m, n, data)  
-    
     def __init__(self, m: int, n: int):
         self.blasfeo_dmat = c_prmt_create_prmt_mat(m, n)  
+    
+    def set(self, data: POINTER(c_double)):
+        m = self.blasfeo_dmat.m 
+        n = self.blasfeo_dmat.n 
+
+        c_prmt_set_prmt_mat(self.blasfeo_dmat, data)  
     
     def print(self):
         c_prmt_print_prmt_mat(self)
--- prometeo/linalg/prmt_mat_blasfeo_wrapper.py
+++ prometeo/linalg/prmt_mat_blasfeo_wrapper.py
@@ -1,23 +1,11 @@
 from blasfeo_wrapper import *
 from ctypes import *
 
-#def c_prmt_create_prmt_mat(m: int, n: int, data: POINTER(c_double)):
-#         
-#    size_strmat = bw.blasfeo_memsize_dmat(m, n)
-#    memory_strmat = c_void_p() 
-#    bw.v_zeros_align(byref(memory_strmat), size_strmat)
-#
-#    ptr_memory_strmat = cast(memory_strmat, c_char_p)
-#
-#    A = (POINTER(c_double) * 1)()
-#    bw.d_zeros(byref(A), n, n)
-#
-#    sA = blasfeo_dmat()
-#
-#    bw.blasfeo_allocate_dmat(m, n, byref(sA))
-#    bw.blasfeo_create_dmat(m, n, byref(sA), ptr_memory_strmat)
-#    bw.blasfeo_pack_dmat(m, n, data[0], n, byref(sA), 0, 0)
-#    return sA
+def c_prmt_set_prmt_mat(M, data: POINTER(c_double)):
+         
+    m = M.m
+    n = M.n
+    bw.blasfeo_pack_dmat(m, n, data, n, byref(M), 0, 0)
 
 def c_prmt_create_prmt_mat(m: int, n: int):
          
@@ -34,7 +22,7 @@ def c_prmt_create_prmt_mat(m: int, n: int):
 
     bw.blasfeo_allocate_dmat(m, n, byref(sA))
     bw.blasfeo_create_dmat(m, n, byref(sA), ptr_memory_strmat)
-    bw.blasfeo_pack_dmat(m, n, data[0], n, byref(sA), 0, 0)
+    bw.blasfeo_pack_dmat(m, n, data, n, byref(sA), 0, 0)
     return sA
 
 def c_prmt_dgemm_nt(A, B, C, D):
