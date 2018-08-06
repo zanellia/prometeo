f5bc83304493c8b4d99d24b71c091a83f8b28157
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-08-06 13:49:25 +0200

    moved pare.py to examples

--- examples/parse.py
+++ examples/parse.py
@@ -1,9 +1,10 @@
+import prometeo
 import ast
 import astpretty
 import typing
-import code_gen
-import code_gen_c
-from source_repr import pretty_source
+# import code_gen
+# import code_gen_c
+# from source_repr import pretty_source
 
 class v(ast.NodeVisitor):
     def generic_visit(self, node):
@@ -34,13 +35,13 @@ module_name = 'code'
 tree = ast.parse(''.join(open(filename)))
 astpretty.pprint(tree)
 
-result  = code_gen_c.to_source(tree, module_name)
+result  = prometeo.cgen.code_gen_c.to_source(tree, module_name)
 
-print("source = \n", pretty_source(result.source))
-print("header = \n", pretty_source(result.header))
+print("source = \n", prometeo.cgen.source_repr.pretty_source(result.source))
+print("header = \n", prometeo.cgen.source_repr.pretty_source(result.header))
 
 dest_file = open('code.c', 'w')
-dest_file.write(pretty_source(result.source))
+dest_file.write(prometeo.cgen.source_repr.pretty_source(result.source))
 
 dest_file = open('code.h', 'w')
-dest_file.write(pretty_source(result.header))
+dest_file.write(prometeo.cgen.source_repr.pretty_source(result.header))
