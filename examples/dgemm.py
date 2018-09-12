e3d6baec4cac008e3bc66c87c92e79811457f9ba
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-09-12 14:58:47 +0200

    testing Mul overload

---
+++ examples/dgemm.py
@@ -0,0 +1,22 @@
+from prometeo.linalg import *
+import sys 
+
+n: int = 10
+
+A: prmt_mat = prmt_mat(n, n)
+A.fill(1.0)
+
+B: prmt_mat = prmt_mat(n, n)
+B.fill(2.0)
+
+C: prmt_mat = prmt_mat(n, n)
+
+prmt_print(C)
+C = A * B
+prmt_print(C)
+# C = A + B
+# C = A - B
+# C = A * B + C
+# C = C + A * B
+# C = A\B
+
