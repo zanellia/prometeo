10b212ea9eaaa0a3e7e33e41f1116de5a2c1e18b
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-09-12 15:10:45 +0200

    adding argument types for all dgead

--- prometeo/linalg/blasfeo_wrapper.py
+++ prometeo/linalg/blasfeo_wrapper.py
@@ -30,6 +30,9 @@ bw.blasfeo_dgemm_tt.argtypes = [c_int, c_int, c_int, c_double,
     POINTER(blasfeo_dmat), c_int, c_int, POINTER(blasfeo_dmat), c_int, c_int, 
     c_double, POINTER(blasfeo_dmat), c_int, c_int, POINTER(blasfeo_dmat), c_int, c_int]
 
+bw.blasfeo_dgead[c_int, c_int, c_double, POINTER(blasfeo_dmat), c_inti, c_int, 
+        POINTER(blasfeo_dmat), c_int, c_int]
+
 bw.blasfeo_dgein1.argtypes = [c_double, POINTER(blasfeo_dmat), c_int, c_int]
 
 def blasfeo_dgemm_nt(m: c_int, n: c_int, k: c_int, alpha: c_double, 
