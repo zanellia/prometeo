d68faea93f4f8885334549c6a54e49c6894b044b
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-10-11 10:44:23 +0200

    polishing

--- prometeo/mem/ast_analyzer.py
+++ prometeo/mem/ast_analyzer.py
@@ -1,24 +1,3 @@
-# import ast
-# class ast_analyzer(ast.NodeTransformer):
-#     def visit_Call(self, node):
-#         node_copy = node
-#         # import pdb; pdb.set_trace()
-#         print(node_copy.func.id)
-#         if hasattr(node_copy, 'callees'):
-#             if node_copy.func.id not in node_copy.callees:
-#                 node_copy.callees.append(node_copy.func.id)
-#                 print(node_copy.callees)
-#         else:
-#             node_copy.callees = [node_copy.func.id]
-#             print(node_copy.callees)
-#         self.generic_visit(node_copy)
-#         return node_copy
-'''
-Get all function calls from a python file
-
-The MIT License (MIT)
-Copyright (c) 2016 Suhas S G <jargnar@gmail.com>
-'''
 import ast
 from collections import defaultdict
 
