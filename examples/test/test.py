5e061364bcd999354f83f311b7adcc9d4b48c86c
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-12-30 17:06:41 +0100

    add Riccati code to test.py

--- examples/test/test.py
+++ examples/test/test.py
@@ -1,11 +1,39 @@
 # UNCOMMENT THESE LINES TO EXECUTE 
 from prometeo import *
 
+sizes: dimv = [[2,2], [2,2], [2,2], [2,2], [2,2]]
+nx: dims = 2
+nu: dims = 2
+N:  dims = 5
+
+class qp_data:
+    C: pmat[nu,nu] = pmat(nx,nu)
+    A: List[pmat, sizes]  = plist(pmat, sizes)
+    l: List[int, 5] = plist(int, 5)
+    B: List[pmat, sizes]  = plist(pmat, sizes)
+    Q: List[pmat, sizes]  = plist(pmat, sizes)
+    R: List[pmat, sizes]  = plist(pmat, sizes)
+    P: List[pmat, sizes]  = plist(pmat, sizes)
+
+    fact: List[pmat, sizes] = plist(pmat, sizes)
+
+    def factorize(self) -> None:
+        res: pmat[nx, nx] = pmat(nx, nx)
+        Bt: pmat[nx, nx] = pmat(nx, nx)
+        for i in range(N):
+            pmt_gemm(self.P[i], self.B[i], res, res)
+            pmat_tran(self.B[i], Bt)
+            pmt_gemm(Bt, res, self.R[i], res)
+            pmt_potrf(res, self.fact[i])
+            # pmt_potrsm(res, self.fact[i])
+
+        return
+
 def main() -> None:
 
     # test assignments
     n: int = 10
-    A: pmat[2,2] = pmat(n, n)
+    M: pmat[2,2] = pmat(n, n)
 
     a : pvec[10] = pvec(10)
     a[1] = 3.0
@@ -13,13 +41,13 @@ def main() -> None:
     d : float = 10.0
 
     # float to pmat
-    A[0,1] = d
+    M[0,1] = d
 
     # float (const) to pmat 
-    A[0,1] = 1.0
+    M[0,1] = 1.0
 
     # pmat to float
-    d = A[0, 1]
+    d = M[0, 1]
 
     # float to pvec
     a[0] = d
@@ -32,18 +60,43 @@ def main() -> None:
 
     # subscripted pmat to pmat 
     for i in range(2):
-        A[0,i] = A[0,i]
+        M[0,i] = M[0,i]
 
     # subscripted pvec to pvec
     a[0] = a[1]
 
     # subscripted pmat to pvec
-    a[1] = A[0, 2]
+    a[1] = M[0, 2]
 
     # subscripted pvec to pmat
-    A[0, 2] = a[1]
+    M[0, 2] = a[1]
 
-# # UNCOMMENT THESE LINES TO EXECUTE 
-# # if __name__ == "__main__":
-    # # execute only if run as a script
-    # # main()
+    # run Riccati code
+    A: pmat[nu,nu] = pmat(nx, nx)
+    B: pmat[nu,nu] = pmat(nx, nu)
+    Q: pmat[nu,nu] = pmat(nx, nx)
+    R: pmat[nu,nu] = pmat(nu, nu)
+    P: pmat[nu,nu] = pmat(nx, nx)
+
+    mat_list: List[pmat, sizes]  = plist(pmat, sizes)
+
+    fact: pmat[nu,nu] = pmat(nx, nx)
+
+    qp : qp_data = qp_data() 
+
+    for i in range(N):
+        qp.A[i] = A
+
+    for i in range(N):
+        qp.B[i] = B
+
+    for i in range(N):
+        qp.Q[i] = Q
+
+    for i in range(N):
+        qp.R[i] = R
+
+    for i in range(N):
+        qp.fact[i] = fact
+
+    qp.factorize()
