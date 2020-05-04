39791b6b582247f62427dd71620cc8c78a789808
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-05-04 11:47:30 +0200

    fix long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ class BinaryDistribution(dist.Distribution):
     def has_ext_modules(foo):
         return True
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='prometeo-dsl',
     version='0.1',
     python_requires='>=3.6, <=3.9',
