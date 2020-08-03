f57416b44f3ed6217c8ad3b99905f7f363db4823
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-08-03 09:54:10 +0200

    update example

--- experimental/blas_api/simple_example.py
+++ experimental/blas_api/simple_example.py
@@ -19,16 +19,26 @@ def main() -> int:
     pmat_print(A)
     print("B:\n")
     pmat_print(B)
+
     print("gemm(A, B, C):\n")
     pmt_gemm(A, B, C)
+    pmat_print(C)
+
+    for i in range(r):
+        for j in range(r):
+            C[i, j] = 5.0
+
+    print("gemm(A, B, C, beta=1.0):\n")
+    pmt_gemm(A, B, C, beta=1.0)
     pmat_print(C)
 
     D: pmat = pmat(r, r)
     pmt_gemm(A, B, C, D)
     print("gemm(A, B, C, D):\n")
     pmat_print(C)
-    pmt_gemm(A, B.T, C, alpha=1.0, beta=0.1)
-    print("gemm(A, B.T, C, alpha=1.0, beta=0.1):\n")
+
+    pmt_gemm(A, B.T, C, alpha=0.5, beta=0.5)
+    print("gemm(A, B.T, C, alpha=0.0, beta=0.1):\n")
     pmat_print(C)
 
     return 0
