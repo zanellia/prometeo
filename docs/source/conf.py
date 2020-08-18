430fe16e87c78fc867d27d1af0804f5b74f1c877
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-08-18 14:54:24 +0200

    do not force rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
-import sphinx_rtd_theme
-
 # -- Project information -----------------------------------------------------
 
 project = 'prometeo'
@@ -76,14 +73,6 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-
-
-extensions = [
-    "sphinx_rtd_theme",
-]
-
-html_theme = "sphinx_rtd_theme"
-
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
